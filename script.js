a76ef6c0e9ac4a038b136b7c5e9dd4808bd02ac9
sintuciitis <sintijako222@gmail.com>
2022-02-24 14:52:46 +0200

    Herons

--- script.js
+++ script.js
@@ -12,5 +12,38 @@ function nolasa (m1, m2, m3){
         console.log(false);
         return false
     }
-
 }
+
+    function perimetrs(m1, m2, m3) {
+        const m = nolasa();
+        m1 = m.m1;  m2 = m.m2;  m3 = m.m3;
+        console.log(m1, m2, m3);
+        const p = m1 + m2 + m3;
+        console.log(p);
+        return (p);
+    }
+
+    function pusper(m1, m2, m3) {
+        const pusper = perimetrs(m1, m2, m3)/2;
+        console.log(pusper);
+        return(pusper);
+    }
+
+    function laukums(m1, m2, m3) {  //laukumu aprēķin pēc Herona formulas
+        const pusper = perimetrs(m1, m2, m3)/2;
+        let tlaukums = Math.sqrt(
+            pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3))
+        );
+        console.log(tlaukums);
+        return tlaukums;
+    }
+
+    //Pārbauda vai divu malu summa ir lielāka par trešo malu
+
+    function irTrijsturis(m1, m2, m3){
+        if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2){
+            return true;
+        } else {
+            return false;
+        }
+    }
